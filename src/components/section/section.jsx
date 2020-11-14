0d9a72f84f1e325143f7e63e567fab2cfb5817ed
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-14 17:39:35 +0900

    [FEAT] article component + only box(except video)

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -5,10 +5,48 @@ import Sectionarticle from './sectionarticle';
 import './section.css'
 
 class Section extends Component {
+  state = {
+    projects : [
+      { 
+        groupNumber: 1,
+        title: '아이캠퍼스+', 
+        class: '1분반',
+        groupName: '브실골플레이어',
+        members: ['컴퓨터교육과 강준우', '컴퓨터교육과 김태현', '컴퓨터교육과 김희동', '컴퓨터교육과 송지훈'],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        pdfURL: 'images/test.jpg',
+        likeCount: 48,
+      },
+      { 
+        groupNumber: 2,
+        title: '러닝페어 아카이빙', 
+        class: '2분반',
+        groupName: '우리팀레전드',
+        members: ['경영학과 홍길동', '경영학과 xxx', '경영학과 ooo', '경영학과 uuu', '경영학과 www'],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        pdfURL: 'images/test.jpg',
+        likeCount: 100,
+      },
+      { 
+        groupNumber: 3,
+        title: 'CMD 프로젝트', 
+        class: '3분반',
+        groupName: '집에보내주시조',
+        members: ['---', '---', '---', '---'],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        pdfURL: 'images/test.jpg',
+        likeCount: 100,
+      },
+    ]
+  } 
+
   render() {
     return (
       <section>
         <Sectionheader />
+        {this.state.projects.map(project => (
+        <Sectionarticle key={project.groupNumber} project={project} />
+        ))}
       </section>
     );
   }
--- src/components/section/sectionarticle.jsx
+++ src/components/section/sectionarticle.jsx
@@ -1,14 +1,67 @@
 import React, { Component } from 'react';
 import './section.css'
+import './sectionarticle.css'
 
 class Sectionarticle extends Component {
   render() {
+    const {pdfURL, title, description, likeCount, groupName, members} = this.props.project;
+
     return (
-      <div>
-        
+      <article>
+
+      {/* 아티클 컨테이너 (비디오 제외) - 컴포넌트로 분리하기 */}
+      <div className="article-container"> 
+
+        {/* 프로젝트 이미지 */}
+        <img src={pdfURL} alt="project-img" className="project-img"/>
+        {/* // 프로젝트 이미지 마무리 */}
+
+        {/* 프로젝트 정보 */}
+        <div className="project-info">
+          <div className="project-title">{title}</div>
+          <div className="project-contents">
+            {/* 여기에 프로젝트 설명이 들어가는 부분 <br/>
+            여러줄 첨부 가능하도록 <br/><br/>
+            넘치면 생략할지 수직으로 스크롤 하게 할지는 <br/>
+            결정해야 할 부분! <br/>  */}
+            {description}
+          </div>
+          <div className="project-like">
+            <button className="project-like-button"><img src="/images/unlike-button.png" alt=""/></button>
+            <span className="project-like-count">{likeCount}</span>
+          </div>
+        </div>
+        {/* // 프로젝트 정보 마무리 */}
+
+        {/* 그룹 정보 */}
+        <div className="group-info">
+
+          {/* 그룹 정보 컨테이너 */}
+          <div className="group-info-container">
+            <div className="groupname-label">팀명</div>
+            <div className="groupname">{groupName}</div>
+            <div className="groupmember-label">팀원</div>
+            <div className="groupmember">
+              {members.map(member => (
+                <div>{member}</div>
+              ))}
+            </div>
+          </div>
+
+          {/* 동영상 버튼  */}
+          <button className="show-video">시연 동영상 보기</button>
+        </div>
+        {/* // 그룹 정보 마무리 */}
+
       </div>
+      {/* // 아티클 컨테이너 마무리 */}
+
+      <video src=""></video>
+
+    </article>
     );
   }
 }
 
 export default Sectionarticle;
+
