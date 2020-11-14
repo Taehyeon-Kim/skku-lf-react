2034ffdbfc240512a5f05b8a288b6b4e540fbfe5
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-14 13:05:03 +0900

    [FEAT] header component (web+mobile)

---
+++ src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="header-container">
+          <img src="/images/logo.png" alt="로고" className="header-logo"/>
+          <div className="header-profile">
+            <span className="header-profile-name">김재현님</span> 
+            <span className="header-profile-msg">환영합니다.</span>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
